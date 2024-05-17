ca1d92e25acdc9e3822de0dba73f9786e080d705
Tim <sakunamary@gmail.com>
2024-05-17 15:14:53 +0800

    update

---
+++ src/TASK_LCD.h
@@ -0,0 +1,24 @@
+#ifndef __TASK_BLE_SERIAL_H__
+#define __TASK_BLE_SERIAL_H__
+
+#include <Arduino.h>
+#include <Wire.h>
+// const uint16_t BT_HREG = 3001;
+// const uint16_t ET_HREG = 3002;
+// const uint16_t AMB_RH_HREG = 3007;
+// const uint16_t AMB_TEMP_HREG = 3008;
+
+// // Modbus Registers Offsets
+// const uint16_t HEAT_HREG = 3003;
+// const uint16_t FAN_HREG = 3004;
+// const uint16_t PID_STATUS_HREG = 3005;
+// const uint16_t PID_SV_HREG = 3006;
+
+
+
+
+
+
+
+
+#endif
