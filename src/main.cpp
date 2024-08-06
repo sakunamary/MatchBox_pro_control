407030aacfbb62cee7ba495a8228984e5c4ecb96
Tim <sakunamary@gmail.com>
2024-08-07 06:17:28 +0800

    updatew

--- src/main.cpp
+++ src/main.cpp
@@ -125,8 +125,8 @@ void setup()
     xSerialReadBufferMutex = xSemaphoreCreateMutex();
     ESP32PWM::allocateTimer(0);
     ESP32PWM::allocateTimer(1);
-    ESP32PWM::allocateTimer(2);
-    ESP32PWM::allocateTimer(3);
+    // ESP32PWM::allocateTimer(2);
+    // ESP32PWM::allocateTimer(3);
 
     // read pid data from EEPROM
 #if defined(DEBUG_MODE)
