0e37bfb15a07d8559857ba6b6d52b80bc9536f63
Tim <sakunamary@gmail.com>
2024-09-13 15:42:14 +0800

    update

--- src/pre-setting/Setting_parm/Setting_parm.ino
+++ src/pre-setting/Setting_parm/Setting_parm.ino
@@ -57,7 +57,7 @@ void setup()
     // ESP32PWM::allocateTimer(0);
     // ESP32PWM::allocateTimer(1);
     // ESP32PWM::allocateTimer(2);
-    //  ESP32PWM::allocateTimer(3);
+    // ESP32PWM::allocateTimer(3);
 
     Serial.begin(BAUDRATE);
 
