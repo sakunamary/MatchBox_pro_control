fe26a42386ada08fa5d7fae4543d1f72dc227fec
Tim <sakunamary@gmail.com>
2024-09-30 22:36:24 +0800

    update

--- src/TASK_read_temp.h
+++ src/TASK_read_temp.h
@@ -73,8 +73,8 @@ void Task_Thermo_get_data(void *pvParameters)
             delay(200);
             MCP.Configuration(1, 16, 1, 1); // MCP3424 is configured to channel i with 18 bits resolution, continous mode and gain defined to 8
             Voltage = MCP.Measure();        // Measure is stocked in array Voltage, note that the library will wait for a completed conversion that takes around 200 ms@18bits
-                                            // BT_TEMP = pid_parm.BT_tempfix + (((Voltage / 1000 * Rref) / ((3.3 * 1000) - Voltage / 1000) - R0) / (R0 * 0.0039083));
-            BT_TEMP = temp_K_cal.Temp_C(Voltage * 0.001, aht20.getTemperature_C()) + pid_parm.BT_tempfix;
+            BT_TEMP = pid_parm.BT_tempfix + (((Voltage / 1000 * Rref) / ((3.3 * 1000) - Voltage / 1000) - R0) / (R0 * 0.0039083));
+            //BT_TEMP = temp_K_cal.Temp_C(Voltage * 0.001, aht20.getTemperature_C()) + pid_parm.BT_tempfix;
             ET_TEMP = BT_TEMP;
             // delay(200);
             // MCP.Configuration(2, 16, 1, 1); // MCP3424 is configured to channel i with 18 bits resolution, continous mode and gain defined to 8
@@ -102,7 +102,7 @@ void Task_Thermo_get_data(void *pvParameters)
 
 #endif
         //检查温度是否达到降温降风
-        if (!PID_TUNNING)
+        if (PID_TUNNING == false)
         {
             if (BT_TEMP < 60 && BT_TEMP > 55)
             {
--- src/config.h
+++ src/config.h
@@ -6,7 +6,7 @@
 
 #define uS_TO_S_FACTOR 1000000 /* Conversion factor for micro seconds to seconds */
 #define BAUDRATE 115200        // serial port baudrate
-#define VERSION "1.1.2a"
+#define VERSION "1.1.2b"
 //  DEBUG_MODE 会在串口输出用于调试的测试反馈信息
 //#define DEBUG_MODE 
 
@@ -36,7 +36,7 @@
 
 
 // pwm setting
-#define PWM_FREQ 2000     //PWM 信号频率 单位：Hz
+#define PWM_FREQ 4000     //PWM 信号频率 单位：Hz
 #define PWM_RESOLUTION 10 // 0-1024 PWM 信号分辨率 10bit 是0-1024级 ，12bit 是 0-4096级，改了主程序的分辨率输出map函数也要相应调整
 
 #define PWM_FAN_MIN 300
