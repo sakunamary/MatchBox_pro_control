774aec28f3f07d4413c6c533b0eace2ca5835a69
Tim <sakunamary@gmail.com>
2024-11-04 11:08:02 +0800

    update

--- src/TASK_read_temp.h
+++ src/TASK_read_temp.h
@@ -181,7 +181,7 @@ void Task_Thermo_get_data(void *pvParameters)
         if (xSemaphoreTake(xThermoDataMutex, timeOut) == pdPASS) // 给温度数组的最后一个数值写入数据
         {
             // 封装BLE 协议
-            sprintf(temp_data_buffer_ble, "#0,%d,%d,%d,%d,%4.2f;\n",(int)round(ET_TEMP), (int)round(BT_TEMP), levelOT1, levelIO3, pid_sv);
+            sprintf(temp_data_buffer_ble, "#0,%d,%d,%d,%d,%4.2f;\n", (int)round(ET_TEMP), (int)round(BT_TEMP), levelOT1, levelIO3, pid_sv);
             xQueueSend(queue_data_to_BLE, &temp_data_buffer_ble, xIntervel);
             xTaskNotify(xTASK_data_to_BLE, 0, eIncrement); // send notify to TASK_data_to_HMI
             memset(&temp_data_buffer_ble, '\0', BLE_BUFFER_SIZE);
@@ -464,10 +464,11 @@ void Task_PID_autotune(void *pvParameters)
                 default:
 
                     break;
-                } //case ending
-            }// for ending 
+                } // case ending
+            } // for ending
         }
     }
+    delay(3000);
     esp_restart();
     vTaskSuspend(xTask_PID_autotune);
 }
