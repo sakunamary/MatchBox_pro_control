9b0d8bc5a0c8218f702bfccfde7c76a23ab8fbc1
Tim <sakunamary@gmail.com>
2024-11-11 17:23:51 +0800

    Update TASK_read_temp.h

--- src/TASK_read_temp.h
+++ src/TASK_read_temp.h
@@ -201,7 +201,7 @@ void Task_Thermo_get_data(void *pvParameters)
         // PID ON:ambient,chan1,chan2,  heater duty, fan duty, SV
         if (xSemaphoreTake(xThermoDataMutex, timeOUT) == pdPASS) // 给温度数组的最后一个数值写入数据
         {
-            sprintf(temp_data_buffer_ble, "#0,0,%d,%d,%d,%4.2f;\n", (int)round(BT_TEMP), levelOT1, levelIO3, pid_sv);
+            sprintf(temp_data_buffer_ble, "#0.00,0.00,%4.2f,%d,%d,%4.2f;\n", (int)round(BT_TEMP), levelOT1, levelIO3, pid_sv);
             xQueueSend(queue_data_to_BLE, &temp_data_buffer_ble, xIntervel);
             xTaskNotify(xTASK_data_to_BLE, 0, eIncrement); // send notify to TASK_data_to_HMI
         }
